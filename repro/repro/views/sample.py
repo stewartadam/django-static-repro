aef15fad1f03b7a8435cdf6418967fa257864974
Stewart Adam <s.adam@diffingo.com>
2022-09-24 10:19:14 -0700

    add pipenv and gunicorn setup

--- repro/repro/views/sample.py
+++ repro/repro/views/sample.py
@@ -10,9 +10,9 @@ def foo(request):
     from django.template import engines
     django_engine = engines['django']
     from django.conf import settings
-    print(settings.FORCE_SCRIPT_NAME)
-    print(settings.STATIC_URL) # Note: in settings.py it is 'static/' but the output here will include FORCE_SCRIPT_NAME
-    print(django_engine.from_string("{% load static %}{% static 'test_file' %}").render())
+    html = ""
+    html += "FORCE_SCRIPT_NAME: %s\n" % settings.FORCE_SCRIPT_NAME
+    html += "STATIC_URL: %s\n" % settings.STATIC_URL # Note: in settings.py it is 'static/' but the output here will include FORCE_SCRIPT_NAME
+    html += "call static for test_file: %s\n" % django_engine.from_string("{% load static %}{% static 'test_file' %}").render()
 
-    html = 'foo'
     return HttpResponse(html)
